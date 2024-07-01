472c91549b8be909ccbde7ac6acfcb9379418bdb
Vlad Subbotin <samediacc666@gmial.com>
2024-07-01 21:21:47 +1100

    git init

---
+++ components/layout/LayoutAside.vue
@@ -0,0 +1,47 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+const links = [
+	['mdi-inbox-arrow-down', 'Inbox'],
+	['mdi-send', 'Send'],
+	['mdi-delete', 'Trash'],
+	['mdi-alert-octagon', 'Spam'],
+]
+
+const drawer = ref(null)
+</script>
+
+<template>
+	<v-navigation-drawer v-model="drawer">
+
+		<v-sheet
+			class="pa-4"
+			color="gray-lighten-4"
+		>
+			<v-avatar
+				class="mb-4"
+				color="grey-darken-1"
+				size="64"
+			/>
+			<div>
+				samediacc666@gmail.com
+			</div>
+
+			<v-divider></v-divider>
+
+			<v-list>
+				<v-list-item
+					v-for="[icon, text] in links"
+					:key="icon"
+					:prepend-icon="icon"
+					:title="text"
+					link
+				></v-list-item>
+			</v-list>
+
+		</v-sheet>
+	</v-navigation-drawer>
+</template>
+
+<style scoped lang='scss'>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+	<v-app>
+		<v-layout class="rounded rounded-md">
+			<layout-aside />
+
+			<v-main class="d-flex align-center justify-center">
+				Main content
+			</v-main>
+		</v-layout>
+	</v-app>
+</template>
+
+<style scoped lang='scss'>
+</style>
