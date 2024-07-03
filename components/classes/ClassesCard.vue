6090a79de74e4d4a4955e398db535a9fcd08551f
Vlad Subbotin <samediacc666@gmial.com>
2024-07-03 15:35:14 +1100

    add classes common info

--- components/classes/ClassesCard.vue
+++ components/classes/ClassesCard.vue
@@ -2,6 +2,7 @@
 import { Classes } from "~/types/classes";
 import { useClassesItemStore } from "~/stores/classes/classes.item";
 import { storeToRefs } from "pinia";
+import { getDiceString } from "~/helpers/utils";
 
 const props = withDefaults(defineProps<{
 	index: Classes.ClassesTypes.ClassIndex
@@ -18,6 +19,53 @@ const { classItem, isLoading } = storeToRefs(classItemStore)
 		<v-card-title>
 			{{ classItem.name }}
 		</v-card-title>
+
+		<v-row>
+			<v-col cols="4">
+				<v-card color="primary" class="mt-2 mx-2 fill-height">
+					<v-card-title>
+						Hit points
+					</v-card-title>
+					<v-card-text>
+						Hit	Dice: {{ getDiceString(1, classItem.hit_die) }} per {{ classItem.name.toLowerCase() }} level
+					</v-card-text>
+					<v-card-text>
+						Hit	Points at 1st	Level: {{ classItem.hit_die }} + your Constitution modifier
+					</v-card-text>
+					<v-card-text>
+						Hit	Points at Higher Levels: {{ getDiceString(1, classItem.hit_die) }} (or	{{ classItem.hit_die / 2 + 1 }}) + your Constitution modifier per	{{ classItem.name.toLowerCase() }}	level	after	1st
+					</v-card-text>
+				</v-card>
+			</v-col>
+			<v-col cols="4">
+				<v-card color="primary" class="mt-2 mx-2 fill-height">
+					<v-card-title>
+						Proficiencies
+					</v-card-title>
+					<ul class="ps-4 ml-4">
+						<li v-for="prof in classItem.proficiencies" :key="prof.key">
+							{{ prof.name }}
+						</li>
+					</ul>
+				</v-card>
+			</v-col>
+			<v-col cols="4">
+				<v-card color="primary" class="mt-2 mx-2 fill-height">
+					<v-card-title>
+						Equipment
+					</v-card-title>
+					<v-card-text>
+						You	start	with the	following	equipment, in	addition to	the	equipment	granted	by your	background:
+					</v-card-text>
+					<ul class="ps-4 ml-4">
+						<li v-for="equip in classItem.starting_equipment_options" :key="equip.desc">
+							{{ equip.desc }}
+						</li>
+					</ul>
+				</v-card>
+			</v-col>
+		</v-row>
+
 		<classes-levels v-if="!isLoading" :class-item="classItem" />
 	</v-tabs-window-item>
 </template>
